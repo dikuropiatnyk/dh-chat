d1d97c56eb26bdc5bbbe23d41617730daae5fa73
Danylo Kuropiatnyk <dikuropiatnyk@spscommerce.com>
2024-03-03 22:10:07 +0200

    First TCP server tryouts

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,9 +1,61 @@
 package main
 
 import (
-	"fmt"
+	"io"
+	"log"
+	"net"
 )
 
+type DHServer struct {
+	addrress string
+	listener net.Listener
+}
+
+func NewDHServer(addrress string) *DHServer {
+	return &DHServer{addrress: addrress}
+}
+
+func (s *DHServer) Start() {
+	listner, err := net.Listen("tcp", s.addrress)
+	if err != nil {
+		log.Fatalln("Bootup error:", err)
+		return
+	}
+	log.Println("DHServer is starting at", s.addrress)
+	defer listner.Close()
+	s.listener = listner
+	s.AcceptConnections()
+}
+
+func (s *DHServer) AcceptConnections() {
+	for {
+		conn, err := s.listener.Accept()
+		if err != nil {
+			log.Println("Connection error:", err)
+			continue
+		}
+		go s.HandleConnection(conn)
+	}
+}
+
+func (s *DHServer) HandleConnection(conn net.Conn) {
+	defer conn.Close()
+	log.Println("Received connection from", conn.RemoteAddr())
+	buffer := make([]byte, 2048)
+	for {
+		n, err := conn.Read(buffer)
+		if err != nil {
+			if err.Error() == io.EOF.Error() {
+				log.Println("Connection closed by client")
+				break
+			}
+			log.Println("Read error: ", err)
+		}
+		log.Printf("%s => %s\n", conn.RemoteAddr(), string(buffer[:n]))
+	}
+}
+
 func main() {
-	fmt.Println("Hello, World! I am Diffie-Hellman server service.")
+	server := NewDHServer("localhost:8080")
+	server.Start()
 }
--- cmd/user/main.go
+++ cmd/user/main.go
@@ -1,9 +1,51 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
+	"net"
+	"os"
 )
 
+type DHUser struct {
+	userAddress   net.Addr
+	serverAddress net.Addr
+}
+
+func (u *DHUser) Connect() {
+	conn, err := net.Dial("tcp", "localhost:8080")
+	if err != nil {
+		log.Fatalln("Connection error:", err)
+		return
+	}
+	defer conn.Close()
+	u.userAddress = conn.LocalAddr()
+	u.serverAddress = conn.RemoteAddr()
+	log.Println("Connected to", u.serverAddress)
+	u.SendData(conn)
+}
+
+func (u *DHUser) SendData(conn net.Conn) {
+	reader := bufio.NewReader(os.Stdin)
+
+	// Read user input and send it to the server
+	fmt.Print("Enter text: ")
+	input, err := reader.ReadBytes('\n')
+	if err != nil {
+		log.Fatalln("Read error:", err)
+	}
+	// Convert the string to a byte slice and send it to the server
+	sent_bytes, err := conn.Write(input)
+	if err != nil {
+		log.Println("Write error:", err)
+		return
+	}
+	log.Printf("Sent %d bytes to %s\n", sent_bytes, u.serverAddress)
+
+}
+
 func main() {
-	fmt.Println("Hello, World! I am Diffie-Hellman user service.")
+	user := DHUser{}
+	user.Connect()
 }
